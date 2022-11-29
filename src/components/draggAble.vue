7fdedccb28c2b60a098bedfd33a9eb43ccc10b22
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2022-11-29 13:43:51 +0300

    Added drag functionalities

---
+++ src/components/draggAble.vue
@@ -0,0 +1,11 @@
+<template>
+  <draggable v-model="todosDrag"></draggable>
+</template>
+<script>
+import { VueDraggableNext } from 'vue-draggable-next'
+export default {
+  components: {
+    draggable: VueDraggableNext,
+  },
+};
+</script>
